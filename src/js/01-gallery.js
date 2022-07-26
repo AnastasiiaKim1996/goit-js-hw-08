c25a2f891ca619ad4d957f9634d144e4e4e9d86b
Анастасия Маслак <96750008+AnastasiiaKim1996@users.noreply.github.com>
2022-07-26 22:34:21 +0300

    add task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const documentGallery = document.querySelector('.gallery');
+
+function createGalleryItems(galleryItems) {
+  return galleryItems.reduce(
+    (acc, item) =>
+      acc +
+      ` <div class="gallery__item">
+             <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </div>`,
+    ''
+  );
+}
+
+const createLi = createGalleryItems(galleryItems);
+documentGallery.insertAdjacentHTML('beforeend', createLi);
+
+// // --- Using the standalone variant ---//
+
+new SimpleLightbox('.gallery a');
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,46 @@
+//  - import -  //
 
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
+
+// --- function for Vimeo-Player  ---//
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', Throttle(saveTime, 1000));
+
+function saveTime(event) {
+  localStorage.setItem(STORAGE_KEY, event.seconds);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+  const localTime = localStorage.getItem(STORAGE_KEY);
+  if (localTime) {
+    player.setCurrentTime(localTime);
+  }
+}
+
+//--- first solution ---//
+
+// function saveTime() {
+//   player
+//     .getCurrentTime()
+//     .then(function (seconds) {
+//       localStorage.setItem(STORAGE_KEY, seconds);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// }
+
+// player.on('timeupdate', Throttle(saveTime, 1000));
+
+// const currentTime = localStorage.getItem(STORAGE_KEY) || 0;
+
+// player.setCurrentTime(currentTime).catch(function (error) {
+//   console.log(error);
+// });
